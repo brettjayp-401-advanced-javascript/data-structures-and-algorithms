afc712adaf965a2a9b9ce755eb7690b453434496
Brett Packard <brettjayp@gmail.com>
2020-01-04 23:07:11 -0800

    forEach challenge 6

--- code-challenges/for-each/challenges-01.test.js
+++ code-challenges/for-each/challenges-01.test.js
@@ -102,8 +102,10 @@ The inventory is formatted like this:
 This function should use forEach to populate your grocery list based on the store's inventory. If the item is available, add it to your list. Return the final list.
 ------------------------------------------------------------------------------------------------ */
 
-const createList = (availableItems) => {
-  // Solution code here...
+const createList = availableItems => {
+  let list = [];
+  availableItems.forEach(value => {if(value.available === true){list.push(value.name)}});
+  return list;
 };
 
 /* ------------------------------------------------------------------------------------------------
