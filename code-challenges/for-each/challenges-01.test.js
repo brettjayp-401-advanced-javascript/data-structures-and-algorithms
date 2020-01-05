9a6f11c4d91f072b91a5e43373b2e8b470cdf6ce
Brett Packard <brettjayp@gmail.com>
2020-01-04 23:15:03 -0800

    forEach challenge 7, all complete

--- code-challenges/for-each/challenges-01.test.js
+++ code-challenges/for-each/challenges-01.test.js
@@ -122,8 +122,20 @@ Iterate over the array using forEach to determine the output based on several ru
 Return the resulting output array.
 ------------------------------------------------------------------------------------------------ */
 
-const fizzbuzz = (arr) => {
-  // Solution code here...
+const fizzbuzz = arr => {
+  let output = [];
+  arr.forEach(value => {
+    if(value % 3 === 0 && value % 5 === 0){
+      output.push('Fizz Buzz');
+    } else if(value % 3 === 0){
+      output.push('Fizz');
+    } else if(value % 5 === 0){
+      output.push('Buzz');
+    } else{
+      output.push(value);
+    };
+  });
+  return output;
 };
 
 /* ------------------------------------------------------------------------------------------------
