6a2bb399d003a22bd034a32f2976a01c3588d4f9
Brett Packard <brettjayp@gmail.com>
2020-05-03 20:47:30 -0700

    added test, edited readmes

---
+++ code-challenges/401n16/data-structures/linkedlist/linked-list.test.js
@@ -0,0 +1,13 @@
+'use strict';
+
+// let ll = require('linkedList');
+
+describe('an empty test', () => {
+  it('will pass', () => {
+    expect(true).toBe(true);
+  })
+})
+
+describe('linked lists creation', () => {
+  it('')
+})
