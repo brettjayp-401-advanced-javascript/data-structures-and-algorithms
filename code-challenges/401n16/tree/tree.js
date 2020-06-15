ef7d2b516fad031662141fb71b53ee7b4276248d
Brett Packard <brettjayp@gmail.com>
2020-06-14 18:48:33 -0700

    started binary search tree

--- code-challenges/401n16/tree/tree.js
+++ code-challenges/401n16/tree/tree.js
@@ -18,38 +18,74 @@ class BinaryTree {
     let rootArr = [];
     let left = [];
     let right = [];
-    rootArr.push(root.val);
-    root.left ? left = this.preOrder(root.left) : true;
-    root.right ? right = this.preOrder(root.right) : true;
+    try{
+      rootArr.push(root.val);
+      if(!rootArr[0] || rootArr[0] === null){throw 'failure with root'};
+      root.left ? left = this.preOrder(root.left) : true;
+      root.right ? right = this.preOrder(root.right) : true;
+    }
+    catch(e){
+      throw e;
+    };
     return [...rootArr, ...left, ...right];
   };
 
-  inOrder(){ // A function that traverses the tree using inOrder depth-first traversal, and returns an array containing all the values in the traversed order
+  inOrder(root = this.root){ // A function that traverses the tree using inOrder depth-first traversal, and returns an array containing all the values in the traversed orderr
     if(!root){return};
     let rootArr = [];
     let left = [];
     let right = [];
-    rootArr.push(root.val);
-    root.left ? left = this.inOrder(root.left) : true;
-    root.right ? right = this.inOrder(root.right) : true;
+    try{
+      rootArr.push(root.val);
+      if(!rootArr[0] || rootArr[0] === null){throw 'failure with root'};
+      root.left ? left = this.inOrder(root.left) : true;
+      root.right ? right = this.inOrder(root.right) : true;
+    }
+    catch(e){
+      throw e;
+    };
     return [...left, ...rootArr, ...right];
   };
 
-  postOrder(){ // A function that traverses the tree using postOrder depth-first traversal, and returns an array containing all the values in the traversed order
+  postOrder(root = this.root){ // A function that traverses the tree using postOrder depth-first traversal, and returns an array containing all the values in the traversed orderr
     if(!root){return};
     let rootArr = [];
     let left = [];
     let right = [];
-    rootArr.push(root.val);
-    root.left ? left = this.preOrder(root.left) : true;
-    root.right ? right = this.preOrder(root.right) : true;
+    try{
+      rootArr.push(root.val);
+      if(!rootArr[0] || rootArr[0] === null){throw 'failure with root'};
+      root.left ? left = this.postOrder(root.left) : true;
+      root.right ? right = this.postOrder(root.right) : true;
+    }
+    catch(e){
+      throw e;
+    };
     return [...left, ...right, ...rootArr];
   };
 };
 
 class BinarySearchTree extends BinaryTree{
   add(val){ // A function that takes in a value as a parameter, and then adds a new Node with that value in the correct locations of the binary search tree
-    ;
+    try{
+      if(!this.root){this.root = new Node(val); return;};
+      let pos = this.root;
+      while(pos){
+        pos.val > val
+          ? !pos.left
+            ? pos.left = new Node(val)
+            : pos = pos.left
+          : pos.val < val
+        ? !pos.right
+          ? pos.right = new Node(val)
+        : pos = pos.right
+      : true;
+      };
+      // return true;
+    }
+    catch(e){
+      throw e;
+    };
   };
 
   contains(val){ // A function that takes in a value as a parameter, and returns true if that value is in the tree, and false if not
--- code-challenges/401n16/tree/tree.test.js
+++ code-challenges/401n16/tree/tree.test.js
@@ -11,7 +11,7 @@ const BinarySearchTree = tree.BinarySearchTree;
 //   });
 // });
 
-describe('binary', () => {
+describe('binary tree', () => {
   let treeA = new BinaryTree();
   treeA.root = new Node('R');
   treeA.root.left = new Node('R.L');
@@ -20,8 +20,16 @@ describe('binary', () => {
   treeA.root.left.right = new Node('R.L.R');
   treeA.root.left.left.right = new Node('R.L.L.R');
   treeA.root.right.right = new Node('R.R.R');
-  console.log(treeA.root)
+  // console.log(treeA.root)
   it('tree and nodes', () => {
     expect(treeA.preOrder()).toStrictEqual(['R', 'R.L', 'R.L.L', 'R.L.L.R', 'R.L.R', 'R.R', 'R.R.R']);
   });
+});
+
+describe('binary search tree', () => {
+  let treeA = new BinarySearchTree();
+  it('search tree add', () => {
+    expect(() => treeA.add(15)).not.toThrow();
+  });
+  console.log(treeA);
 });
